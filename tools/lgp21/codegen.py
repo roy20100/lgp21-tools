238564df22c03f7536ce8a57f2a098ab2f1c1421
Rhys Weatherley <rhys.weatherley@gmail.com>
2026-03-18 19:37:18 +1000

    Code generator backend for assemblers and compilers

---
+++ tools/lgp21/codegen.py
@@ -0,0 +1,313 @@
+
+# Copyright (C) 2026 Rhys Weatherley
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+import sys
+import lgp21.hexadecimal as hexadecimal
+import lgp21.insn as insn
+
+'''
+Converts the decimal form of an address to the hexadecimal form.
+Returns None if the decimal form cannot be converted.
+'''
+def dec_to_hex_address(value):
+    if value < 0 or value > 6363:
+        return None
+    track = int(value / 100)
+    sector = int(value % 100)
+    if track > 63 or sector > 63:
+        return None
+    return track * 64 + sector
+
+'''
+Details about an instruction for the code generator, which may be
+partially instantiated if labels have not yet been resolved.
+'''
+class Instruction:
+    '''
+    Constructs a new instruction at a specific location in the source file.
+    '''
+    def __init__(self, location):
+        self.word = 0
+        self.relocatable = False
+        self.literal = False
+        self.label = None
+        self.location = location
+        self.offset = 0
+
+    '''
+    Sets the order code for this instruction and clear the other fields.
+    '''
+    def set_order(self, order):
+        self.word = order
+        self.relocatable = False
+        self.literal = False
+        self.label = None
+        self.offset = 0
+
+    '''
+    Sets the address field for this instruction to a literal value.
+    '''
+    def set_address(self, address):
+        self.word = (self.word & insn.ORDER_MASK) | ((address << insn.ADDRESS_SHIFT) & insn.ADDRESS_MASK)
+        self.relocatable = False
+        self.literal = False
+        self.label = None
+        self.offset = 0
+
+    '''
+    Sets the address field for this instruction to a label.
+    '''
+    def set_label(self, label):
+        self.label = label
+        if 'value' in label:
+            # Literal value used as the address field.
+            self.relocatable = False
+        else:
+            # Label has a relocatable address or is a forward reference.
+            self.relocatable = True
+        self.literal = False
+        self.offset = 0
+
+    '''
+    Sets this instruction to a literal word value.
+    '''
+    def set_literal(self, word):
+        self.word = word
+        self.relocatable = False
+        self.literal = True
+        self.label = None
+        self.offset = 0
+
+    '''
+    Sets the offset from the label to include in the address field.
+
+    This allows label references like "msg+3", "dest-2", and so on.
+    '''
+    def set_offset(self, value):
+        self.offset = value
+
+    '''
+    Resolve the label reference in this instruction.  Returns False if the
+    label is undefined.
+    '''
+    def resolve(self):
+        if self.label:
+            if 'address' in self.label:
+                # Label is fully resolved.
+                address = self.label['address']
+            elif 'value' in self.label:
+                # Label was defined with an explicit value.
+                address = self.label['value']
+                self.relocatable = False
+            else:
+                return False
+            address += self.offset
+            address = address & 4095
+            self.word = (self.word & ~insn.ADDRESS_MASK) | (address << insn.ADDRESS_SHIFT)
+        return True
+
+    '''
+    Formats this instruction for the final tape image.
+    '''
+    def format(self):
+        if self.literal:
+            return hexadecimal.to_hex(self.word) + "'"
+        if (self.word & 0x80000000) != 0:
+            if self.relocatable:
+                prefix = '800'
+            else:
+                prefix = '80x'
+        elif self.relocatable:
+            prefix = ''
+        else:
+            prefix = 'x'
+        order = (self.word & 0x000F0000) >> 16
+        track = (self.word & insn.TRACK_MASK) >> insn.TRACK_SHIFT
+        sector = (self.word & insn.SECTOR_MASK) >> insn.SECTOR_SHIFT
+        return prefix + hexadecimal.order_chars[order] + ('%02d%02d' % (track, sector)) + "'"
+
+'''
+Support class for generating LGP-21 code from assemblers and compilers.
+'''
+class CodeGenerator:
+    '''
+    Initialises the code generator.
+    '''
+    def __init__(self):
+        self.memory = [None] * 4096
+        self.PC = 0
+        self.labels = {}
+        self.entry_point = None
+        self.relocatable = False
+
+    '''
+    Adds an instruction at the current PC and advances the PC.
+    Returns False if there already was an instruction at the PC.
+    '''
+    def add_instruction(self, insn):
+        if self.memory[self.PC] != None:
+            return False
+        self.memory[self.PC] = insn
+        self.PC = (self.PC + 1) & 4095
+        return True
+
+    '''
+    Gets a label definition for the current location in the program.
+    Returns None if the label was already defined.
+    '''
+    def label_for_PC(self, name, location):
+        if name in self.labels:
+            # We may have seen this label before in a forward reference.
+            label = self.labels[name]
+            if 'address' in label or 'value' in label:
+                # Label is already defined.
+                return None
+            label['address'] = self.PC
+            label['location'] = location
+        else:
+            label = {'name': name, 'address': self.PC, 'location': location}
+            self.labels[name] = label
+        return label
+
+    '''
+    Sets a label to an explicit value.  Used for equate symbols.
+    Returns None if the label was already defined
+    '''
+    def set_label(self, name, value, location):
+        if name in self.labels:
+            # We have seen this label before.  Already defined or forward ref?
+            label = self.labels[name]
+            if 'value' in label or 'address' in label:
+                # Label is already defined.
+                return None
+            label['value'] = value
+            label['location'] = location
+        else:
+            label = {'name': name, 'value': value, 'location': location}
+            self.labels[name] = label
+        return label
+
+    '''
+    Gets a previously defined label or a forward reference to a future label.
+    '''
+    def get_label(self, name, location):
+        if name in self.labels:
+            label = self.labels[name]
+        else:
+            label = {'name': name, 'location': location}
+            self.labels[name] = label
+        return label
+
+    '''
+    Set the entry point label for the program.
+    '''
+    def set_entry_point(self, label):
+        self.entry_point = label
+
+    '''
+    Resolve all labels in all instructions and create the final words
+    to be written to the output tape.  Returns False if there was an error.
+    '''
+    def resolve(self):
+        ok = True
+        for insn in self.memory:
+            if insn != None and not insn.resolve():
+                sys.stderr.write("%s: undefined label `%s'" % (insn.location, insn.label['name']))
+                ok = False
+        if self.entry_point != None:
+            # Check that the entry point label has been resolved.
+            label = self.entry_point
+            if not ('address' in label) and not ('value' in label):
+                sys.stderr.write("%s: undefined label `%s'" % (label['location'], label['name']))
+                ok = False
+        return ok
+
+    '''
+    Converts the final program into a tape image in ASCII.
+    It is assumed that resolve() has already been run successfully.
+    '''
+    def to_tape(self):
+        # Start with an empty tape.
+        tape = ''
+
+        # Output each run of contiguous words.
+        address = 0
+        while address < len(self.memory):
+            # Skip unusued instructions to find the next run.
+            while address < len(self.memory) and self.memory[address] == None:
+                address += 1
+            if address >= len(self.memory):
+                # No more runs in the program.
+                break
+
+            # Write the origin definition for this run.
+            if not self.relocatable:
+                tape += ";000%02d%02d'" % (address / 64, address % 64)
+                tape += "/0000000'"
+                word_count = 2
+            else:
+                tape += "m40k%02d%02d'" % (address / 64, address % 64)
+                word_count = 1
+
+            # Output the words in the run.
+            while address < len(self.memory) and self.memory[address] != None:
+                insn = self.memory[address]
+                if insn.literal:
+                    # Output a run of literal words, maximum of 63 words.
+                    count = 1
+                    while count < 63 and (address + count) < len(self.memory) and self.memory[address + count] != None and self.memory[address + count].literal:
+                        count += 1
+                    if word_count >= 8:
+                        tape += '\n'
+                        word_count = 0
+                    tape += ",00000%02d'" % count
+                    word_count += 1
+                    while count > 0:
+                        if word_count >= 8:
+                            tape += '\n'
+                            word_count = 0
+                        tape += self.memory[address].format()
+                        word_count += 1
+                        address += 1
+                        count -= 1
+                else:
+                    # Output a standard instruction.
+                    if word_count >= 8:
+                        tape += '\n'
+                        word_count = 0
+                    tape += insn.format()
+                    word_count += 1
+                    address += 1
+            if word_count != 0:
+                tape += '\n'
+
+        # Jump to the entry point if we have one.
+        if self.entry_point != None and not self.relocatable:
+            if 'address' in self.entry_point:
+                address = self.entry_point['address']
+            elif 'value' in self.entry_point:
+                address = self.entry_point['value']
+            address = address & 4095
+            tape += ".000%02d%02d'\n" % (address / 64, address % 64)
+            pass
+
+        # Tape is ready to go.
+        return tape
